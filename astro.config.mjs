4d1e8e2949d5285c629740fabf2942701295c017
Bola Ojewale <me@bolaojewale.com>
2025-04-26 08:36:52 +0000

    initial deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify";
+import cloudflare from '@astrojs/cloudflare';
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
@@ -12,26 +12,26 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarcocavallo.com/",
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        "https://gianmarcocavallo.com/sitemap-index.xml",
-        "https://gianmarcocavallo.com/sitemap-0.xml",
-      ],
-    }),
-    solidJs(),
-    UnoCSS({ injectReset: true }),
-    icon(),
-    svelte(),
-  ],
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
-  output: "server",
-  adapter: netlify({ edgeMiddleware: true }),
-  vite: {
-    assetsInclude: "**/*.riv",
+  // site: "https://bolaojewale.com/",
+  // integrations: [
+  //   sitemap(),
+  //   robotsTxt({
+  //     sitemap: [
+  //       "https://bolaojewale.com/sitemap-index.xml",
+  //       "https://bolaojewale.com/sitemap-0.xml",
+  //     ],
+  //   }),
+  //   solidJs(),
+  //   UnoCSS({ injectReset: true }),
+  //   icon(),
+  //   svelte(),
+  // ],
+  // markdown: {
+  //   remarkPlugins: [remarkReadingTime],
+  // },
+  output: 'server',
+  adapter: cloudflare(),
+  image: {
+    service: { entrypoint: 'astro/assets/services/compile' }, // Use compile instead of sharp for Cloudflare compatibility
   },
 });
