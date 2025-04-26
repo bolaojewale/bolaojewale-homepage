83435003cfc2acefe6b49f5aad7c270c31b15a99
Bola Ojewale <me@bolaojewale.com>
2025-04-26 08:51:14 +0000

    hopefully this fixed deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,34 @@
 import { defineConfig } from "astro/config";
-// import sitemap from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap";
 import cloudflare from '@astrojs/cloudflare';
-// import robotsTxt from "astro-robots-txt";
-// import UnoCSS from "@unocss/astro";
-// import icon from "astro-icon";
+import robotsTxt from "astro-robots-txt";
+import UnoCSS from "@unocss/astro";
+import icon from "astro-icon";
 
-// import solidJs from "@astrojs/solid-js";
-// import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
+import solidJs from "@astrojs/solid-js";
+import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
 
-// import svelte from "@astrojs/svelte";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: "https://bolaojewale.com/",
-  // integrations: [
-  //   sitemap(),
-  //   robotsTxt({
-  //     sitemap: [
-  //       "https://bolaojewale.com/sitemap-index.xml",
-  //       "https://bolaojewale.com/sitemap-0.xml",
-  //     ],
-  //   }),
-  //   solidJs(),
-  //   UnoCSS({ injectReset: true }),
-  //   icon(),
-  //   svelte(),
-  // ],
-  // markdown: {
-  //   remarkPlugins: [remarkReadingTime],
-  // },
+  site: "https://bolaojewale.com/",
+  integrations: [
+    sitemap(),
+    robotsTxt({
+      sitemap: [
+        "https://bolaojewale.com/sitemap-index.xml",
+        "https://bolaojewale.com/sitemap-0.xml",
+      ],
+    }),
+    solidJs(),
+    UnoCSS({ injectReset: true }),
+    icon(),
+    svelte(),
+  ],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
   output: 'server',
   adapter: cloudflare(),
   image: {
