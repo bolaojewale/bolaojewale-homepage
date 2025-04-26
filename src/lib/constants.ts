3344d5a7d87fd2b6c7bbd0e4781c62846bc75904
Bola Ojewale <me@bolaojewale.com>
2025-04-26 10:44:58 +0000

    personalization

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,10 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
+  github: "https://github.com/cybergardner",
+  linkedin: "https://www.linkedin.com/in/bola-ojewale/",
+  medium: "https://cybergardner.medium.com/",
   discord: "https://discordapp.com/users/163300027618295808",
   dribble: "https://dribbble.com/Ladvace_Jace",
-  email: "contact@gianmarcocavallo.com<",
+  email: "me@bolaojewale.com",
 };
 
 export const loaderAnimation = [
