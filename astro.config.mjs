a49de2ff857562b50b985bbba701bbaa96415fb1
Bola Ojewale <me@bolaojewale.com>
2025-04-26 08:40:36 +0000

    initial deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
+// import sitemap from "@astrojs/sitemap";
 import cloudflare from '@astrojs/cloudflare';
-import robotsTxt from "astro-robots-txt";
-import UnoCSS from "@unocss/astro";
-import icon from "astro-icon";
+// import robotsTxt from "astro-robots-txt";
+// import UnoCSS from "@unocss/astro";
+// import icon from "astro-icon";
 
-import solidJs from "@astrojs/solid-js";
-import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
+// import solidJs from "@astrojs/solid-js";
+// import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
 
-import svelte from "@astrojs/svelte";
+// import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
